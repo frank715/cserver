df1715ab893e7be9578a62699d085eea22aaca43
frank715 <factfrank6@gmail.com>
2024-07-19 13:40:47 +0530

    issue

--- app.js
+++ app.js
@@ -63,8 +63,8 @@ app.use(bodyParser.json({ limit: "35mb" }));
 // }
 
 // view engine setup
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "jade");
+// app.set("views", path.join(__dirname, "views"));
+// app.set("view engine", "jade");
 
 app.use(logger("dev"));
 app.use(express.json());
